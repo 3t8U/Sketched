89cd6cceae88478f43a447332fec7a155c1b479a
Nate Powers <russellsproutz@gmail.com>
2020-08-03 10:35:22 -0700

    finihsed test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import stress from './stress.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Sketch'd Out </p>
+        <img src={stress}></img>
+        <p>Under pressure... "How is the interview going", he asked. "Well, let me tell you. . ."......</p>
+
       </header>
     </div>
   );
